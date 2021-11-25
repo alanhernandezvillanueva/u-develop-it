92151b5fb6d679ddb4c94c5968176bf284916b31
alanhernandezvillanueva <79774478+alanhernandezvillanueva@users.noreply.github.com>
2021-11-25 12:21:18 -0800

    connected server to data basa and created a query that retunrs all the data from candidates

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
+
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Tracy123!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+db.query(`SELECT * FROM candidates`, (err, rows) =>{
+    console.log(rows);
+})
 
-
-
-
-
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-  });
-  // Default response for any other request (Not Found)
+  // Default response for any other request (Not Found) must be the last one(at the bottom)
 app.use((req, res) => {
     res.status(404).end();
   });
